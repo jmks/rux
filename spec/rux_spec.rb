37b7c5a6170eef189cdb67f5729dd07957e60132
Jason Schweier <jasons@nulogy.com>
2019-04-16 23:41:53 -0400

    Add spec using the builder

--- spec/rux_spec.rb
+++ spec/rux_spec.rb
@@ -5,5 +5,22 @@ RSpec.describe Rux do
 
       expect(regexp).to be_a(Regexp)
     end
+
+    describe "building a regexp for words with length >= 3 and ending with 'at'" do
+      subject(:regexp) do
+        Rux.rux do
+          bow
+          one_or_more letters
+          literal "at"
+          eow
+        end
+      end
+
+      it { is_expected.to match "bat" }
+      it { is_expected.to match "flat" }
+      it { is_expected.to_not match "map" }
+      it { is_expected.to_not match "at" }
+      it { is_expected.to_not match "attach" }
+    end
   end
 end
